7563744a944e6a70d5d0983e644e5ecb5dd42354
L9A1 <t0in4@yahoo.com>
2026-05-15 04:06:07 +0900

    added reranking model

--- src/main/java/com/t0in4/RidesRetrievalAugmentor.java
+++ src/main/java/com/t0in4/RidesRetrievalAugmentor.java
@@ -3,23 +3,43 @@ package com.t0in4;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.scoring.onnx.OnnxScoringModel;
 import dev.langchain4j.rag.DefaultRetrievalAugmentor;
 import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.aggregator.ContentAggregator;
+import dev.langchain4j.rag.content.aggregator.ReRankingContentAggregator;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.redis.RedisEmbeddingStore;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.function.Supplier;
 
 @ApplicationScoped
 public class RidesRetrievalAugmentor implements Supplier<RetrievalAugmentor> {
     @Inject
     EmbeddingStore<TextSegment> store;  // ✅ CDI from extension
-    @Inject @Named("local-embed")
-    EmbeddingModel model;        // Your GigaChat producer
+    @Inject
+    @Named("local-embed")
+    EmbeddingModel model;
+
+    //reramking model and tokenizer https://huggingface.co/BAAI/bge-reranker-large/tree/main
+    @ConfigProperty(name = "embedding.model.path", defaultValue = "protected")
+    String modelDir;
+    Path modelPath = Paths.get(modelDir, "model.onnx").toAbsolutePath().normalize();
+    Path tokenizerPath = Paths.get(modelDir, "tokenizer.json").toAbsolutePath().normalize();
+    OnnxScoringModel scoringModel = new OnnxScoringModel(modelPath.toString(), tokenizerPath.toString());
+    // Content aggregator adds/removes/sorts content
+    ContentAggregator contentAggregator = ReRankingContentAggregator.builder()
+            .scoringModel(scoringModel)
+            .minScore(0.8)
+            .build();
 
     @Override
     public RetrievalAugmentor get() {  // Lazy init
@@ -36,6 +56,7 @@ public class RidesRetrievalAugmentor implements Supplier<RetrievalAugmentor> {
         );*/
         return DefaultRetrievalAugmentor.builder()
                 .contentRetriever(new HeightAwareRetriever(store, model))
+                .contentAggregator(contentAggregator) // Injects the content aggregator to sort texts based on the scoring model
                 .build();
     }
 }
